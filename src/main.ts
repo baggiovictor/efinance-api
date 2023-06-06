151c1b896d28bc064b0f6c2a81999a56e2ac1587
victor.baggio <baggiobaggiovictor@gmail.com>
2023-06-06 16:44:06 -0300

    fix-cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('User API')
     .setDescription('API for managing users')
